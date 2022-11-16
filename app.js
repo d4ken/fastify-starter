de8bc57a84685ac1959491099f13c508b9fb02be
Daiki Shikencho <d-shikencho@techqiao.co.jp>
2022-11-16 22:31:49 +0900

    viewsを追加

--- app.js
+++ app.js
@@ -1,25 +1,38 @@
-'use strict'
+'use strict';
 
-const path = require('path')
-const AutoLoad = require('@fastify/autoload')
+const path = require('path');
+const AutoLoad = require('@fastify/autoload');
+const POV = require('point-of-view');
+const { Liquid } = require('liquidjs');
+
+const engine = new Liquid({
+  root: path.join(__dirname, 'views'),
+  extname: '.liquid',
+});
 
 module.exports = async function (fastify, opts) {
   // Place here your custom code!
 
   // Do not touch the following lines
 
+  fastify.register(POV, {
+    engine: {
+      liquid: engine,
+    },
+  });
+
   // This loads all plugins defined in plugins
   // those should be support plugins that are reused
   // through your application
   fastify.register(AutoLoad, {
     dir: path.join(__dirname, 'plugins'),
-    options: Object.assign({}, opts)
-  })
+    options: Object.assign({}, opts),
+  });
 
   // This loads all plugins defined in routes
   // define your routes in one of these
   fastify.register(AutoLoad, {
     dir: path.join(__dirname, 'routes'),
-    options: Object.assign({}, opts)
-  })
-}
+    options: Object.assign({}, opts),
+  });
+};
--- routes/root.js
+++ routes/root.js
@@ -1,7 +1,12 @@
-'use strict'
+'use strict';
+
+
 
 module.exports = async function (fastify, opts) {
   fastify.get('/', async function (request, reply) {
-    return { root: true }
-  })
-}
+    return { root: true };
+  });
+  fastify.get('/liquid', async function (request, reply) {
+    return reply.view("/views/index", { title: "タイトル"});
+  });
+};
